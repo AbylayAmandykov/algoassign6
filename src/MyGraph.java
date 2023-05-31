225db72fd4a3accf74a10c9c7fabe65af45ca566
Abylay <abylay0505@gmail.com>
2023-06-01 02:35:41 +0600

    new class added

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 public class MyGraph<Vertex> {
     private Map<Vertex, List<Vertex>> list;
     public MyGraph() {
