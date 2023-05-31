7eb71671dbfc0f6dd1353df63f7eef458074548c
Abylay <abylay0505@gmail.com>
2023-05-31 17:39:59 +0600

    constructor

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,3 +1,9 @@
-public class MyGraph {
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+public class MyGraph<Vertex> {
+    private Map<Vertex, List<Vertex>> list;
+    public MyGraph() {
+        list = new HashMap<>();
+    }
 }
