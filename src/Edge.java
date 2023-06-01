d55a0df6ebdffeda93d1d8921e0234cf9cefafb2
Abylay <abylay0505@gmail.com>
2023-06-01 11:46:26 +0600

    docs added

--- src/Edge.java
+++ src/Edge.java
@@ -2,26 +2,63 @@ public class Edge<Vertex> {
     private Vertex source;
     private Vertex dest;
     private Double weight;
+    /**
+     * Constructs an edge between two vertices with a specified weight.
+     *
+     * @param source the source vertex of the edge
+     * @param dest   the destination vertex of the edge
+     * @param weight the weight of the edge
+     */
     public Edge(Vertex source, Vertex dest, Double weight) {
         this.source = source;
         this.dest = dest;
         this.weight = weight;
     }
+    /**
+     * Retrieves the source vertex of the edge.
+     *
+     * @return the source vertex
+     */
     public Vertex getSource() {
         return source;
     }
+    /**
+     * Sets the source vertex of the edge.
+     *
+     * @param source the source vertex to set
+     */
     public void setSource(Vertex source) {
         this.source = source;
     }
+    /**
+     * Retrieves the destination vertex of the edge.
+     *
+     * @return the destination vertex
+     */
     public Vertex getDest() {
         return dest;
     }
+    /**
+     * Sets the destination vertex of the edge.
+     *
+     * @param dest the destination vertex to set
+     */
     public void setDest(Vertex dest) {
         this.dest = dest;
     }
+    /**
+     * Retrieves the weight of the edge.
+     *
+     * @return the weight of the edge
+     */
     public Double getWeight() {
         return weight;
     }
+    /**
+     * Sets the weight of the edge.
+     *
+     * @param weight the weight to set
+     */
     public void setWeight(Double weight) {
         this.weight = weight;
     }
