f2d8942ac3c3f5187e96370700b37bb5cd5f131f
Abylay <abylay0505@gmail.com>
2023-06-01 11:51:10 +0600

    docs added

--- src/Vertex.java
+++ src/Vertex.java
@@ -3,10 +3,21 @@ import java.util.Map;
 public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjVertices;
+    /**
+     * Constructs a vertex with the specified data
+     *
+     * @param data the data associated with the vertex
+     */
     public Vertex(V data) {
         this.data = data;
         this.adjVertices = new HashMap<>();
     }
+    /**
+     * Adds an adjacent vertex with a specified weight
+     *
+     * @param dest   the adjacent vertex
+     * @param weight the weight of the edge connecting this vertex to the adjacent vertex
+     */
     public void addAdjVertex(Vertex<V> dest, double weight) {
         adjVertices.put(dest, weight);
     }
